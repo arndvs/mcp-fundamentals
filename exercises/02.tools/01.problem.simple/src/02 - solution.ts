475421abedf5eea48a92094ebb638d2e2d5d9de7
Aaron Davis <ad@arndvs.com>
2025-09-26 11:22:03 -0700

    add solution for problem 2 - register tools

---
+++ exercises/02.tools/01.problem.simple/src/02 - solution.ts
@@ -0,0 +1,55 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+
+const server = new McpServer(
+	{
+		name: 'epicme',
+		title: 'EpicMe',
+		version: '1.0.0',
+	},
+	{
+		
+	
+		// 🐨 add a capabilities object with a tools property that is an empty object
+		capabilities: {
+			tools: {},
+		},
+		instructions: 'This lets you solve math problems.',
+	},
+)
+
+// 🐨 add a tool to the server with the server.registerTool API
+// - the name should be 'add'
+// - the config object should include a user-facing title and an llm-facing description explaining what it can be used to do (add one and two)
+// - the callback should return a standard text response that says "The sum of 1 and 2 is 3."
+server.registerTool(
+	'add',
+	{
+		//user-facing title
+		title: 'Add the numbers one and two',
+		//llm-facing description & possibly user-facing description - 
+		// these are the situations where the tool can be used - 
+		 // be descriptive of what your tool can do, include examples of, like, if you give me these parameters, then this is what's going to happen
+		description: 'Add the numbers 1 and 2 ',
+	},
+	async () => {
+		return {
+			// types of content - text, image, audio, video, and resources.
+			// text is the most common type of content.
+			content: [{ type: 'text', text: 'The sum of 1 and 2 is 3.' }],
+		}
+	},
+)
+
+
+
+async function main() {
+	const transport = new StdioServerTransport()
+	await server.connect(transport)
+	console.error('EpicMe MCP Server running on stdio')
+}
+
+main().catch((error) => {
+	console.error('Fatal error in main():', error)
+	process.exit(1)
+})
